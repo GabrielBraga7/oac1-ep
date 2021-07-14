68211d6a1564f019ff1effec5a156fc064c67251
Gigademolidor <39094702+Gigademolidor@users.noreply.github.com>
2021-07-13 23:06:30 -0300

    Adding comments, fixing bugs and refactoring

--- Main.java
+++ Main.java
@@ -1,33 +1,46 @@
 public class Main{
     public static void main(String[] args){
         //calcular a maior P-ésima potência de X que torne X^P menor ou igual a um dado N.
-        double n = 0.33; // 1/4
-        if(n <= 0){
+        double n = 9;
+        double x = 0.2;
+        
+        if(n <= 0 || x <= 0){
             System.out.println("Nao!"); //nenhum número elevado a uma potência pode ser menor ou igual a zero.
         }
         else{
-            System.out.println(calculaP(3, n));
+            System.out.println(calculaP(x, n));
         }
     }
 
     public static int calculaP(double x, double n){
         double res = x;
-        if(x > n){
-            return calculaPMenorIgualA0(x, n, res);
+        if(x <= n){
+            return calculaPMaiorQue0(x, n, res, x);
         }
         else{
-            return calculaPMaiorQue0(x, n, res);
+            return calculaPMenorIgualA0(x, n, res);
         }
+        
     } 
     public static int calculaPMenorIgualA0(double x, double n, double res){
         int p = 1;
-        while(res > n){
-            res *= (1/x);
-            p--;
+        if(x > 1){
+            while(res > n){
+                res *= (1/x);
+                p--;
+            }
+        }
+        else{
+            res = (1/res);
+            p = 0;
+            while(res < n){
+                res *= (1/x);
+                p--;
+            }
         }
         return p;
     }
-    public static int calculaPMaiorQue0(double x, double n, double res){
+    public static int calculaPMaiorQue0(double x, double n, double res, double factorMult){
         int p = 0;
         while(res <= n){
             res *= x;
--- ep1-rascunho.asm
+++ ep1-rascunho.asm
@@ -1,83 +1,132 @@
+#       Programa em linguagem Assembly que, ao receber um numero N
+#       e outro numero x (como entrada pelo usuario), calcula o
+#       maior grau a que eh possivel elevar x sem que o resultado
+#       ultrapasse o valor de N.
 ########## EP1
 .text
 .globl main
 main:
-        l.s $f12,$x                       # inicia o total
-        l.s $f13,$n
-        l.s $f14, $z
-        l.s $f17, $o
-        c.le.s $f12, $f14
-        bc1t printError
-        c.le.s $f13, $f14
-        bc1t printError
-        jal calculaP
-        la $a0, ($s0)
-        j printInt                # chama o fatorial
+        la $a0, $mensagemEntrada
+        jal printStr                    #pede para o usuario inserir
+        jal readDouble                  #o valor do numero x.
+        mov.d $f12,$f0                  #armazena x no registrador f12.
+        
+        la $a0, $mensagemEntrada2
+        jal printStr                    #pede para o usuario inserir
+        jal readDouble                  #o valor do numero N.
+        mov.d $f14,$f0                  #armazena N no registrador f14.
+
+        l.d $f16, $zeroDouble   #atribui 0 ao registrador f16.
+        l.d $f18, $oneDouble    #atribui 1 ao registrador f18.
+
+        c.le.d $f12, $f16       #verifica se x <= 0. caso verdadeiro,
+        bc1t printError         #imprime uma mensagem de erro.
+
+        c.le.d $f14, $f16       #verifica se N <= 0. caso verdadeiro,
+        bc1t printError         #imprime uma mensagem de erro.
+
+        c.eq.d $f12, $f18       #verifica se x = 1. caso verdadeiro,
+        bc1t printError         #imprime uma mensagem de erro.
+
+        jal calculaP            #executa a funcao calculaP.
+        
+        la $a0, $sucesso        #confirma o sucesso da operacao.
+        jal printStr
+
+        la $a0, ($s0)           #imprime o valor de p obtido na operacao.
+        jal printInt
+
+        j exit           
 
 calculaP:
-        l.s $f15, $x 
-        c.le.s $f12,$f13
-        bc1t calculaPMaiorQue0
+        add.d $f20, $f16, $f12  #cria o registrador f20
+        c.lt.d $f12, $f18
+        bc1t calculaPMenorIgualA0
+        c.le.d $f12, $f14
+        bc1t calculaPMaiorQue0     
+
 
 calculaPMenorIgualA0:
         li $s0, 1
         j loopPMenorIgualA0
+
+loopPMenorIgualA0:
+        c.le.d $f20,$f14
+        bc1t retorna
+        div.d $f22, $f18, $f12
+        mul.d $f20, $f20, $f22
+        j decrementaP
+
+decrementaP:
+        addi $s0, $s0, -1
+        j loopPMenorIgualA0
+
+
+
 
 calculaPMaiorQue0:
         li $s0, 0
         j loopPMaiorQue0
 
 loopPMaiorQue0:
-        c.lt.s $f13,$f15
+        c.lt.d $f14,$f20
         bc1t retorna
-        mul.s $f15, $f15, $f12
+        mul.d $f20, $f20, $f12
         j incrementaP
-
-loopPMenorIgualA0:
-        c.le.s $f15,$f13
-        bc1t retorna
-        div.s $f16, $f17, $f12
-        mul.s $f15, $f15, $f16
-        j decrementaP
-
+        
 incrementaP:
         addi $s0, $s0, 1
         j loopPMaiorQue0
-decrementaP:
-        addi $s0, $s0, -1
-        j loopPMenorIgualA0
+
+
+printInt:
+        li $v0,1                       # imprime o resultado
+        j syscallAndReturn
+
+printError:
+        la $a0, $erro
+        jal printStr
+	j exit
+
+printStr:
+        li $v0,4                        # imprime o resultado
+        j syscallAndReturn
+
+
+
+readDouble:
+        li $v0, 7
+        j syscallAndReturn
+
+
+
+syscallAndReturn:
+        syscall
+        j retorna
+
+
 
 retorna:
-       jr $ra                                # retorna
+       jr $ra   # retorna
+
+
 
 exit:
         li $v0,10                        # termina
         syscall
 
-printError:
-        la $a0, $erro
-        j printStr
-
-printStr:
-        li $v0,4                        # imprime o resultado
-        j print
-
-print:
-        syscall
-        j exit
-
-printInt:
-        li $v0,1                       # imprime o resultado
-        j print
-
 .data
-$z: .float 0.0
-$o: .float 1.0
-$n: .float 0.25
-$x: .float 2.0 
-    $erro:
-        .asciiz "Não é possível efetuar a operação, já que n <= 0"
-     $texto:
-        .asciiz "calculaPMaiorQue0"       
-    $texto2:
-        .asciiz "calculaPMenorIgualA0"     
+$zeroDouble: .double 0.0
+$oneDouble: .double 1.0
+
+$mensagemEntrada:
+        .asciiz "\nDigite o valor de x: "
+
+$mensagemEntrada2:
+        .asciiz "\nDigite o valor de n: "
+
+$erro:
+        .asciiz "\nNão é possível efetuar a operação, já que ou n <= 0, ou x <= 0 ou x = 1"
+
+$sucesso:
+        .asciiz "\nO valor de 'p' obtido foi: "
