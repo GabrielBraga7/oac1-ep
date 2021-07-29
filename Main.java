7560c617698760bfe30a4ed57d2c5e41c5e61735
Gigademolidor <39094702+Gigademolidor@users.noreply.github.com>
2021-07-28 23:57:37 -0300

    Consertando pequeno bug + adicionando uso de exception para indicar erro 'p' infinito

--- Main.java
+++ Main.java
@@ -14,8 +14,14 @@ public class Main{
             System.out.println("Não é possível efetuar a operação, já que ou n <= 0, ou x <= 0 ou 1");
         }
         else{
-            int p = calculaP(x, n);    //executa a funcao calcula p
-            System.out.println(p == Integer.MAX_VALUE ? "P = Infinito" : "P = " + p);
+            int p;
+            try{
+                p = calculaP(x, n);    //executa a funcao calcula p
+                System.out.println("P = " + p);
+            }catch(ArithmeticException e){
+                System.out.println("P é infinito");
+            }
+
         }
         sc.close();
     }
@@ -26,7 +32,7 @@ public class Main{
     }
 
     public static int calculaP(double x, double n){
-        if(x > n){
+        if(x >= n){
             if(x < 1) 
                 return calculaPXMaiorN(x, n, 1, 1, x);      //calcula P se x > N e x < 1 (ex: x = 1/2 e N = 1/8).
             else
@@ -34,7 +40,7 @@ public class Main{
         }
         else{
             if(x < 1)
-                return Integer.MAX_VALUE;   //P aumenta mas x^P diminui, ou seja, x^P sempre será menor do que N (ex: x = 1/4 e N = 1/2)
+                throw new ArithmeticException("P é infinito");   //P aumenta mas x^P diminui, ou seja, x^P sempre será menor do que N (ex: x = 1/4 e N = 1/2)
             else
                 return calculaPXMenorIgualN(x, n, 0, 1, x); //calcula P se x <= N e x >= 1 (ex: x = 2 e N = 8).
         }
